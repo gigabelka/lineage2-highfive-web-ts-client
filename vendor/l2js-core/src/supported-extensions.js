18ad2e207133c54249e38470a71a28b97deda04f
gigabelka <gigabelka@gmail.com>
2026-09-07 19:48:08 +0500

    feat: add SUPPORTED_EXTENSIONS constant for Unreal engine file types

--- vendor/l2js-core/src/supported-extensions.js
+++
@@ -1,6 +0,0 @@
-/**
- * @type {SupportedExtensions_T[]}
- */
-export const SUPPORTED_EXTENSIONS = ["UNR", "UTX", "USX", "UAX", "U", "UKX", "USK", "U"];
-
-export default { SUPPORTED_EXTENSIONS };
---
+++ vendor/l2js-core/src/supported-extensions.ts
@@ -0,0 +1,13 @@
+export const SUPPORTED_EXTENSIONS: C.SupportedExtensions_T[] = [
+    "UNR",
+    "UTX",
+    "USX",
+    "UAX",
+    "U",
+    "UKX",
+    "USK",
+    "U"
+];
+
+export default { SUPPORTED_EXTENSIONS };
+
